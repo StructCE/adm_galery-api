921bc6bcf0527fb3357c8c370546e9c8deec37a0
David Mendes <davidgmendes@hotmail.com>
2021-06-16 17:55:17 -0300

    testes login e logout

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < ApplicationController
-  acts_as_token_authentication_handler_for User, only: %i[show update destroy index logout]
+  acts_as_token_authentication_handler_for User, only: %i[show update destroy index logout], fallback_to_devise: false
 
+  before_action :require_login, only: %i[show update destroy index]
   before_action :check_if_exists, only: :show
 
   def create
@@ -55,10 +56,12 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def logout
-    sign_out(current_user)
-    head(:ok)
-  rescue StandardError => e
-    render json: { message: e }, status: :bad_request
+    if current_user.presence
+      sign_out(current_user)
+      head(:ok)
+    else
+      head(:bad_request)
+    end
   end
 
   private
@@ -77,4 +80,9 @@ class Api::V1::UsersController < ApplicationController
   #   head(:forbidden) unless
   #     current_user.authentication_token == User.find_by(email: 'admin@admin.com').authentication_token
   # end
+
+  def require_login
+    head(:unauthorized) unless current_user.presence
+  end
+
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,7 +1,71 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::Users', type: :request do
+
+  before do
+    User.delete_all
+    @user = create(:user)
+  end
+
+  def valid_login
+    post '/api/v1/login', params: { email: 'teste@teste.com', password: 'TesteSenha' }
+  end
+
+  describe 'POST /login' do
+    context 'when user logs in correctly' do
+      it 'returns a successful response' do
+        valid_login
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when user miss login params' do
+      it 'returns unauthorized when email is wrong' do
+        post '/api/v1/login', params: { email: 'teste.com', password: 'TesteSenha' }
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns unauthorized when password is wrong' do
+        post '/api/v1/login', params: { email: 'teste@teste.com', password: 'Senha' }
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe 'POST /logout' do
+    context 'when user is not logged' do
+      it 'returns bad request' do
+        post '/api/v1/logout'
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context 'when user is logged' do
+      before do
+        sign_in @user
+      end
+      it 'returns ok' do
+        post '/api/v1/logout'
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'GET /index' do
+    context 'when user is not logged' do
+      it 'does not show index' do
+        get '/api/v1/users/index'
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+    context 'when user is logged' do
+      before do
+        sign_in @user
+      end
+      it 'shows index' do
+        get '/api/v1/users/index'
+        expect(response).to have_http_status(:ok)
+      end
+    end
   end
 end
