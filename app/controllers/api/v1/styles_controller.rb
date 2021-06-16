db9fda65fbef9f091a16b27cf1e7c32c6187d22f
Andre Macedo <andremacedo.pv@gmail.com>
2021-06-16 11:23:24 -0300

    :white_check_mark: Request test for Styles

--- app/controllers/api/v1/styles_controller.rb
+++ app/controllers/api/v1/styles_controller.rb
@@ -1,37 +1,34 @@
 class Api::V1::StylesController < ApplicationController
   def index
     styles = Style.all
-    render json: styles
+    render json: styles, status: :ok
   end
 
   def create
     style = Style.new(style_params)
     style.save!
-    render json: style
-    head(:created)
+    render json: style, status: :created
     rescue StandardError => e
-      head(:unprocessable_entity)
+      head(:bad_request)
   end
 
   def show
     style = Style.find(params[:id])
-    render json: style
+    render json: style, status: :ok
   end
 
   def update
     style = Style.find(params[:id])
     style.update!(style_params)
-    render json: style
+    render json: style, status: :ok
     rescue StandardError => e
-      head(:unprocessable_entity)
+      head(:bad_request)
   end
 
   def destroy
     style = Style.find(params[:id])
     style.destroy
     head(:no_content)
-    rescue StandardError => e
-      head(:forbidden)
   end
 
   private
--- spec/requests/api/v1/styles_spec.rb
+++ spec/requests/api/v1/styles_spec.rb
@@ -1,6 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Styles", type: :request do
-  describe "GET /index" do
+  # Valid style params
+  let(:new_style) do
+    { title: 'Nome', description: 'Muito maneiro!' }
+  end
+
+  # Bad style params
+  let(:bad_style) do
+    { title: '', description: 'oi' }
+  end
+
+  # Base style created
+  let(:style) do
+    Style.create!(
+      title: "Titulo",
+      description: "Muito maneiro!"
+    )
+  end
+
+  context 'GET / (index)' do
+    it 'should render all styles' do
+      get '/api/v1/styles/'
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+  end
+
+  context 'POST /create' do
+    it 'is valid' do
+      post '/api/v1/styles/create', params: { style: new_style }
+      expect(response).to have_http_status(:created)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+
+    it 'without valid params' do
+      post '/api/v1/styles/create', params: { style: bad_style }
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  context 'GET /:id (show)' do
+    it 'is valid' do
+      get "/api/v1/styles/#{style.id}"
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+  end
+
+  context 'POST /:id/update' do
+    it 'is valid' do
+      post "/api/v1/styles/#{style.id}/update", params: { style: new_style }
+      expect(response).to have_http_status(:ok)
+      expect { style.reload }.to change(style, :title).to('Nome')
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+
+    it 'without valid params' do
+      post "/api/v1/styles/#{style.id}/update", params: { style: bad_style }
+      expect(response).to have_http_status(:bad_request)
+    end
+  end
+
+  context 'DELETE /:id/destroy' do
+    it 'should delete' do
+      delete "/api/v1/styles/#{style.id}/destroy"
+      expect(response).to have_http_status(:no_content)
+      expect{ style.reload }.to raise_exception(ActiveRecord::RecordNotFound)
+    end
   end
 end
