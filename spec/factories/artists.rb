60a64b596aa8fda615ac251c93c5f403f13399df
iiiqwertyiii <nicpabe@gmail.com>
2021-06-15 23:08:22 -0300

    model e controllers prontas (sem rspec)

---
+++ spec/factories/artists.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :artist do
+    name { "MyString" }
+    biography { "MyText" }
+    birthdate { "2021-06-15" }
+    deathdate { "2021-06-15" }
+    birthplace { "MyString" }
+  end
+end
