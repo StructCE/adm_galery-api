65ba367097f0efcab1b9a55060fe85a75a61e7f1
Andre Macedo <andremacedo.pv@gmail.com>
2021-06-16 10:26:16 -0300

    :sparkles: Api CRUD for styles

--- app/controllers/api/v1/styles_controller.rb
+++ app/controllers/api/v1/styles_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::StylesController < ApplicationController
+  def index
+    styles = Style.all
+    render json: styles
+  end
+
+  def create
+    style = Style.new(style_params)
+    style.save!
+    render json: style
+    head(:created)
+    rescue StandardError => e
+      head(:unprocessable_entity)
+  end
+
+  def show
+    style = Style.find(params[:id])
+    render json: style
+  end
+
+  def update
+    style = Style.find(params[:id])
+    style.update!(style_params)
+    render json: style
+    rescue StandardError => e
+      head(:unprocessable_entity)
+  end
+
+  def destroy
+    style = Style.find(params[:id])
+    style.destroy
+    head(:no_content)
+    rescue StandardError => e
+      head(:forbidden)
+  end
+
+  private
+  def style_params
+    params.require(:style).permit(
+      :title,
+      :description
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      scope 'styles' do
+        get '', to: 'styles#index'
+        post 'create', to: 'styles#create'
+        get ':id', to: 'styles#show'
+        post ':id/update', to: 'styles#update'
+        delete ':id/destroy', to: 'styles#destroy'
+      end
+    end
+  end
 end
