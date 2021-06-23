66b31fea8f083f1ca992955d3e6857be61102ae7
David Mendes <davidgmendes@hotmail.com>
2021-06-23 19:17:20 -0300

    :white_check_mark: testes controller da biblioteca

--- app/controllers/api/v1/libraries_controller.rb
+++ app/controllers/api/v1/libraries_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::LibrariesController < ApplicationController
   acts_as_token_authentication_handler_for User, fallback_to_devise: false
   before_action :require_login
+  before_action :user_has_library?, only: %i[add_paintings remove_paintings]
 
   def create
     library = Library.new(user_id: current_user.id, painting_ids: params[:painting_ids])
@@ -33,10 +34,12 @@ class Api::V1::LibrariesController < ApplicationController
 
   def remove_paintings
     library = current_user.library
+
     paintings = if params[:painting_ids].is_a?(Array)
                   library.painting_ids - params[:painting_ids]
                 else
                   library.painting_ids.delete(params[:painting_ids])
+                  library.painting_ids
                 end
     library.update!(painting_ids: paintings)
     render json: library, status: :ok
@@ -45,15 +48,25 @@ class Api::V1::LibrariesController < ApplicationController
   end
 
   def show
-    library = current_user.library
-    render json: library
-  rescue StandardError => e
-    render json: { Error: e }, status: :bad_request
+    if current_user.library
+      begin
+        library = current_user.library
+        render json: library
+      rescue StandardError => e
+        render json: { Error: e }, status: :bad_request
+      end
+    else
+      head(:not_found)
+    end
   end
 
   private
 
   def library_params
     params.require(:library).permit(:user_id, :painting_ids)
+  end
+
+  def user_has_library?
+    head(:bad_request) unless current_user.library
   end
 end
