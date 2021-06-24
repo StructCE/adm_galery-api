a484e8fff029b3e766eb9ab0504e73f8274fb048
Andre Macedo <andremacedo.pv@gmail.com>
2021-06-23 22:11:21 -0300

    :white_check_mark: Added uniqueness test to join table

--- spec/models/recommendation_painting_spec.rb
+++ spec/models/recommendation_painting_spec.rb
@@ -15,5 +15,17 @@ RSpec.describe RecommendationPainting, type: :model do
     context 'when join table has no recommendation' do
       it { expect(build(:recommendation_painting, recommendation: nil)).to be_invalid }
     end
+
+    context 'when the join is not unique' do
+      it 'expects new join to not be valid' do
+        artist = create(:artist)
+        style = create(:style)
+        painting = create(:painting, artist: artist, style: style)
+        recommendation = create(:recommendation)
+        join = create(:recommendation_painting, painting: painting, recommendation: recommendation)
+        new_join = build(:recommendation_painting, painting: painting, recommendation: recommendation)
+        expect(new_join).not_to be_valid
+      end
+    end
   end
 end
