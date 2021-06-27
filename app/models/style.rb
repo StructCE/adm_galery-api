fcfdca9c78dba2d296d3950ee51a242c927d3b9a
David Mendes <davidgmendes@hotmail.com>
2021-06-27 12:41:20 -0300

    :adhesive_bandage: adicionando destroy recursivo para artista e estilo

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,5 +1,5 @@
 class Style < ApplicationRecord
-  has_many :paintings
+  has_many :paintings, dependent: :destroy
   validates :title, :description, presence: true
   validates :title, length: { minimum: 3 }
   validates :description, length: { minimum: 10 }
