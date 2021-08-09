b48ed6a2a11142422a3d46e2462607e5c33c8fb0
iiiqwertyiii <nicpabe@gmail.com>
2021-08-09 10:23:59 -0300

    :bug: consertando um scope errado

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,14 @@ Rails.application.routes.draw do
         get 'index', to: 'users#index'
         delete 'destroy', to: 'users#destroy'
         patch 'update', to: 'users#update'
+      end
 
-        scope 'library' do
-          post 'create', to: 'libraries#create'
-          delete 'destroy', to: 'libraries#destroy'
-          patch 'add_paintings', to: 'libraries#add_paintings'
-          patch 'remove_paintings', to: 'libraries#remove_paintings'
-          get 'show', to: 'libraries#show'
-        end
+      scope 'library' do
+        post 'create', to: 'libraries#create'
+        delete 'destroy', to: 'libraries#destroy'
+        patch 'add_paintings', to: 'libraries#add_paintings'
+        patch 'remove_paintings', to: 'libraries#remove_paintings'
+        get 'show', to: 'libraries#show'
       end
 
       post 'login', to: 'users#login'
