539e1c6d0e516aea5275f9e5802cccb7bff29422
David Mendes <davidgmendes@hotmail.com>
2021-08-17 05:54:40 -0300

    :children_crossing: mandando biblioteca no serializer de usuario

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :email, :bio, :image_url, :admin, :authentication_token, :confidential
+  attributes :id, :name, :email, :bio, :image_url, :admin, :authentication_token, :confidential, :library
+
+  has_one :library, serializer: LibrarySerializer
 
   def image_url
     if object.image.attached?
